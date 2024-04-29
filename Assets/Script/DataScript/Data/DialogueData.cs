3ffe07b1f12a926666376325ea08b379ac3cf140
chyun7114 <chyun7114@naver.com>
2024-04-29 14:08:19 +0900

    데이터 getter, setter 추가
    getter, setter 사용법도 추가했습니다

---
+++ Assets/Script/DataScript/Data/DialogueData.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DialogueData : MonoBehaviour
+{
+    [SerializeField]
+    private int dialogueId;
+    private int nextDialogueId;
+    private int npcId;
+    private int questId;
+    
+    private string dialogueString;
+    private string npcName;
+    
+    // getter, setter 설정 부분
+    
+    /*
+     * C#의 getter, setter는 
+     * getId, setId 따로 두는게 아니라 한꺼번에 둘 수 있어서
+     * getId = player.Id
+     * player.Id = 20
+     * 이런 방식으로 사용하셔야 됩니다
+     */
+    public int DialogueId
+    {
+        get => dialogueId;
+        set => dialogueId = value;
+    }
+
+    public int NextDialogueId
+    {
+        get => nextDialogueId;
+        set => nextDialogueId = value;
+    }
+    
+    public int NPCId
+    {
+        get => npcId;
+        set => npcId = value;
+    }
+
+    public int QuestId
+    {
+        get => questId;
+        set => questId = value;
+    }
+
+    public string DialogueString
+    {
+        get => dialogueString;
+        set => dialogueString = value;
+    }
+
+    public string NpcName
+    {
+        get => NpcName;
+        set => NpcName = value;
+    }
+}
