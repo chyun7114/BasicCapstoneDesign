ed4afe378abcf98c828105ce6118560e1bad9374
chyun7114 <chyun7114@naver.com>
2024-04-29 14:22:35 +0900

    데이터스크립트 완성

--- Assets/Script/DataScript/Data/NPCData.cs
+++ Assets/Script/DataScript/Data/NPCData.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class NPCData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private int npcId;
+    private string npcName;
+    
+    
+    /*
+     * C#의 getter, setter는
+     * getId, setId 따로 두는게 아니라 한꺼번에 둘 수 있어서
+     * getId = player.Id
+     * player.Id = 20
+     * 이런 방식으로 사용하셔야 됩니다
+     */
+    public int NpcId
     {
-        
+        get => npcId;
+        set => npcId = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public string NpcName
     {
-        
+        get => npcName;
+        set => npcName = value;
     }
 }
--- Assets/Script/DataScript/Data/QuestData.cs
+++ Assets/Script/DataScript/Data/QuestData.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class QuestData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int questId;
+    private int nextQuestId;
+    private int previousQuestId;
+    private int npcId;
+    private string questName;
+    private string questDetail;
+
+    public int QuestId
     {
-        
+        get => questId;
+        set => questId = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int NextQuestId
     {
-        
+        get => nextQuestId;
+        set => nextQuestId = value;
+    }
+
+    public int PreviousAuestId
+    {
+        get => previousQuestId;
+        set => previousQuestId = value;
+    }
+
+    public int NpcId
+    {
+        get => npcId;
+        set => npcId = value;
+    }
+
+    public string QuestName
+    {
+        get => questName;
+        set => questName = value;
+    }
+
+    public string QuestDetail
+    {
+        get => questDetail;
+        set => questDetail = value;
     }
 }
