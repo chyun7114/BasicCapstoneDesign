76d937192a9ab4ec591c044dc69ae3d05ecfc6f7
chyun7114 <chyun7114@naver.com>
2024-06-04 16:08:23 +0900

    navMesh 적용중

---
+++ Assets/Script/CarNavScript.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarNavScript : MonoBehaviour
+{
+    public Transform[] waypoints;  // Waypoints 배열
+    private int currentWaypointIndex = 0;
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        MoveToNextWaypoint();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance && !agent.pathPending)
+        {
+            MoveToNextWaypoint();
+        }
+    }
+    
+    void MoveToNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+            return;
+
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+    }
+}
